d8b40fb4b1e67c38e25f8f82d3ea07edc5d86a25
twosilly <twosilly@foxmail.com>
2024-09-28 09:40:30 +0800

    1.update deps config

--- deps/MPFR/MPFR.cmake
+++ deps/MPFR/MPFR.cmake
@@ -1,56 +1,22 @@
 set(_srcdir ${CMAKE_CURRENT_LIST_DIR}/mpfr)
-set(_dstdir ${DESTDIR}/usr/local)
 
 if (MSVC)
-    set(_output  ${_dstdir}/include/mpfr.h
-                 ${_dstdir}/include/mpf2mpfr.h
-                 ${_dstdir}/lib/libmpfr-4.lib 
-                 ${_dstdir}/bin/libmpfr-4.dll)
+    set(_output  ${DESTDIR}/include/mpfr.h
+                 ${DESTDIR}/include/mpf2mpfr.h
+                 ${DESTDIR}/lib/libmpfr-4.lib 
+                 ${DESTDIR}/bin/libmpfr-4.dll)
 
     add_custom_command(
         OUTPUT  ${_output}
-        COMMAND ${CMAKE_COMMAND} -E copy ${_srcdir}/include/mpfr.h ${_dstdir}/include/
-        COMMAND ${CMAKE_COMMAND} -E copy ${_srcdir}/include/mpf2mpfr.h ${_dstdir}/include/
-        COMMAND ${CMAKE_COMMAND} -E copy ${_srcdir}/lib/win${DEPS_BITS}/libmpfr-4.lib ${_dstdir}/lib/
-        COMMAND ${CMAKE_COMMAND} -E copy ${_srcdir}/lib/win${DEPS_BITS}/libmpfr-4.dll ${_dstdir}/bin/
+        COMMAND ${CMAKE_COMMAND} -E copy ${_srcdir}/include/mpfr.h ${DESTDIR}/include/
+        COMMAND ${CMAKE_COMMAND} -E copy ${_srcdir}/include/mpf2mpfr.h ${DESTDIR}/include/
+        COMMAND ${CMAKE_COMMAND} -E copy ${_srcdir}/lib/win${DEPS_BITS}/libmpfr-4.lib ${DESTDIR}/lib/
+        COMMAND ${CMAKE_COMMAND} -E copy ${_srcdir}/lib/win${DEPS_BITS}/libmpfr-4.dll ${DESTDIR}/bin/
     )
 
     add_custom_target(dep_MPFR SOURCES ${_output})
 
 else ()
-
-    set(_gmp_ccflags "-O2 -DNDEBUG -fPIC -DPIC -Wall -Wmissing-prototypes -Wpointer-arith -pedantic -fomit-frame-pointer -fno-common")
-    set(_gmp_build_tgt "${CMAKE_SYSTEM_PROCESSOR}")
-
-    if (APPLE)
-        if (${CMAKE_SYSTEM_PROCESSOR} MATCHES "arm")
-            set(_gmp_build_arch aarch64)
-        else ()
-            set(_gmp_build_arch ${CMAKE_SYSTEM_PROCESSOR})
-        endif()
-        if (IS_CROSS_COMPILE)
-            if (${CMAKE_OSX_ARCHITECTURES} MATCHES "arm")
-                set(_gmp_host_arch aarch64)
-                set(_gmp_host_arch_flags "-arch arm64")
-            elseif (${CMAKE_OSX_ARCHITECTURES} MATCHES "x86_64")
-                set(_gmp_host_arch x86_64)
-                set(_gmp_host_arch_flags "-arch x86_64")
-            endif()
-            set(_gmp_ccflags "${_gmp_ccflags} ${_gmp_host_arch_flags} -mmacosx-version-min=${DEP_OSX_TARGET}")
-            set(_gmp_build_tgt --build=${_gmp_build_arch}-apple-darwin --host=${_gmp_host_arch}-apple-darwin)
-        else ()
-            set(_gmp_ccflags "${_gmp_ccflags} -mmacosx-version-min=${DEP_OSX_TARGET}")
-            set(_gmp_build_tgt "--build=${_gmp_build_arch}-apple-darwin")
-        endif()
-    elseif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-        if (${CMAKE_SYSTEM_PROCESSOR} MATCHES "arm")
-            set(_gmp_ccflags "${_gmp_ccflags} -march=armv7-a") # Works on RPi-4
-            set(_gmp_build_tgt armv7)
-        endif()
-        set(_gmp_build_tgt "--build=${_gmp_build_tgt}-pc-linux-gnu")
-    else ()
-        set(_gmp_build_tgt "") # let it guess
-    endif()
 
     set(_cross_compile_arg "")
     if (CMAKE_CROSSCOMPILING)
@@ -63,7 +29,8 @@ else ()
         URL_HASH SHA256=b9df93635b20e4089c29623b19420c4ac848a1b29df1cfd59f26cab0d2666aa0
         DOWNLOAD_DIR ${DEP_DOWNLOAD_DIR}/MPFR
         BUILD_IN_SOURCE ON
-        CONFIGURE_COMMAND env "CFLAGS=${_gmp_ccflags}" "CXXFLAGS=${_gmp_ccflags}" ./configure ${_cross_compile_arg} --prefix=${DESTDIR}/usr/local --enable-shared=no --enable-static=yes --with-gmp=${DESTDIR}/usr/local ${_gmp_build_tgt}
+        CONFIGURE_COMMAND autoreconf -f -i && 
+                          env "CFLAGS=${_gmp_ccflags}" "CXXFLAGS=${_gmp_ccflags}" ./configure ${_cross_compile_arg} --prefix=${DESTDIR} --enable-shared=no --enable-static=yes --with-gmp=${DESTDIR} ${_gmp_build_tgt}
         BUILD_COMMAND make -j
         INSTALL_COMMAND make install
         DEPENDS dep_GMP
--- deps/OpenCV/OpenCV.cmake
+++ deps/OpenCV/OpenCV.cmake
@@ -4,17 +4,10 @@ else ()
     set(_use_IPP "-DWITH_IPP=OFF")
 endif ()
 
-# get relative path of CMAKE_BINARY_DIR against root source directory
-file(RELATIVE_PATH BINARY_DIR_REL  ${CMAKE_SOURCE_DIR}/.. ${CMAKE_BINARY_DIR})
-
-set(patch_command git apply --verbose --ignore-space-change --whitespace=fix ${CMAKE_CURRENT_LIST_DIR}/0001-vs2022.patch)
-
-
 orcaslicer_add_cmake_project(OpenCV
     URL https://github.com/opencv/opencv/archive/refs/tags/4.6.0.tar.gz
     URL_HASH SHA256=1ec1cba65f9f20fe5a41fda1586e01c70ea0c9a6d7b67c9e13edf0cfe2239277
-    #PATCH_COMMAND git apply --directory ${BINARY_DIR_REL}/dep_OpenCV-prefix/src/dep_OpenCV --verbose --ignore-space-change --whitespace=fix ${CMAKE_CURRENT_LIST_DIR}/0001-vs2022.patch
-    PATCH_COMMAND ${patch_command}
+    PATCH_COMMAND git apply --directory ${BINARY_DIR_REL}/dep_OpenCV-prefix/src/dep_OpenCV --verbose --ignore-space-change --whitespace=fix ${CMAKE_CURRENT_LIST_DIR}/0001-vs2022.patch
     CMAKE_ARGS
     -DBUILD_SHARED_LIBS=0
        -DBUILD_PERE_TESTS=OFF
